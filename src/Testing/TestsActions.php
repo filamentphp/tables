6ff4c484ff56449efe7802d29d6728540927ee95
Dan <git@danharrin.com>
2023-04-15 21:25:30 +0000

    Update & recompile

--- src/Testing/TestsActions.php
+++ src/Testing/TestsActions.php
@@ -157,6 +157,51 @@ class TestsActions
         };
     }
 
+    public function assertTableActionsExistInOrder(): Closure
+    {
+        return function (array $names): static {
+            $livewire = $this->instance();
+            $this->assertActionListInOrder(
+                $names,
+                $livewire->getTable()->getActions(),
+                'table',
+                Action::class,
+            );
+
+            return $this;
+        };
+    }
+
+    public function assertTableHeaderActionsExistInOrder(): Closure
+    {
+        return function (array $names): static {
+            $livewire = $this->instance();
+            $this->assertActionListInOrder(
+                $names,
+                $livewire->getTable()->getHeaderActions(),
+                'table header',
+                Action::class,
+            );
+
+            return $this;
+        };
+    }
+
+    public function assertTableEmptyStateActionsExistInOrder(): Closure
+    {
+        return function (array $names): static {
+            $livewire = $this->instance();
+            $this->assertActionListInOrder(
+                $names,
+                $livewire->getTable()->getEmptyStateActions(),
+                'table empty state',
+                Action::class,
+            );
+
+            return $this;
+        };
+    }
+
     public function assertTableActionVisible(): Closure
     {
         return function (string $name, $record = null): static {
