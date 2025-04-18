70b71869d0c89269e1733e2db05aae05dae0a341
Dan <git@danharrin.com>
2025-04-18 12:09:49 +0000

    Merge pull request #16072 from filamentphp/fix/cell-state-record-key

--- src/Concerns/CanReorderRecords.php
+++ src/Concerns/CanReorderRecords.php
@@ -36,6 +36,7 @@ trait CanReorderRecords
 
             $model = app($this->getTable()->getModel());
             $modelKeyName = $model->getKeyName();
+            $wrappedModelKeyName = $model->getConnection()->getSchemaGrammar()->wrap($modelKeyName);
 
             $model
                 ->newModelQuery()
@@ -43,7 +44,7 @@ trait CanReorderRecords
                 ->update([
                     $orderColumn => DB::raw(
                         'case ' . collect($order)
-                            ->map(fn ($recordKey, int $recordIndex): string => 'when ' . $modelKeyName . ' = ' . DB::getPdo()->quote($recordKey) . ' then ' . ($recordIndex + 1))
+                            ->map(fn ($recordKey, int $recordIndex): string => 'when ' . $wrappedModelKeyName . ' = ' . DB::getPdo()->quote($recordKey) . ' then ' . ($recordIndex + 1))
                             ->implode(' ') . ' end'
                     ),
                 ]);
