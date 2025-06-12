920204cd5ec1550209cf398fea8dba3dece979de
Dan <git@danharrin.com>
2025-06-12 15:01:25 +0000

    Merge pull request #16491 from filamentphp/fix/windows-import-file-validation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1244,7 +1244,9 @@
                     @endif
                 </x-filament-tables::table>
             @elseif ($records === null)
-                <div class="h-32"></div>
+                <div class="flex h-32 items-center justify-center">
+                    <x-filament::loading-indicator class="h-8 w-8" />
+                </div>
             @elseif ($emptyState = $getEmptyState())
                 {{ $emptyState }}
             @else
